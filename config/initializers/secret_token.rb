5ee6bdd65cbafed8871b1dc0736d386612caef20
cw <conway@xerpi.com>
2013-08-03 02:02:33 +0200

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Beach::Application.config.secret_key_base = 'f5d7b7103581f35beb0f29c80b4db3b477d6ddf6a811f97eb337eec6ad9613150ea36c12e3206daf809b402dc01c0e2683d4dc48711ceb541b8d703cd5c81405'
