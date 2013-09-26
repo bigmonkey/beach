95012752ac892dbe4cc15cee255ffd5fc5897ce0
bigmonkey <conway@xerpi.com>
2013-09-26 13:11:35 -0400

    removed homes controlloer, homepage uses CMS, styled homepage

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Beach::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'homes#index'
+  #root 'homes#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
