d73042c9b19dff3ec4f09efc65f3f5368afc276c
bigmonkey <conway@xerpi.com>
2013-09-27 01:54:06 -0400

    added private features via application.yml

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Beach::Application.config.secret_key_base = 'f5d7b7103581f35beb0f29c80b4db3b477d6ddf6a811f97eb337eec6ad9613150ea36c12e3206daf809b402dc01c0e2683d4dc48711ceb541b8d703cd5c81405'
+Beach::Application.config.secret_key_base = ENV['COOKIE_SECRET_TOKEN']
