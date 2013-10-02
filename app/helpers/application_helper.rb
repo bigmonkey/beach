14c7cd7bc95ab169ee0d703cef14ec0961e2852f
bigmonkey <conway@xerpi.com>
2013-10-01 21:33:32 -0400

    created breadcrumb, fixed dropdown hover styling, change homepage image, masked PW for cms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+	def breadcrumb_pages
+		
+		# initialize page to current page 
+		page = @cms_page
+		pages = []
+		# loop through pages until top of hierarchy where parent_id is nil
+		until page.parent_id.nil?
+			pages << page.id
+			page = @cms_site.pages.find_by id: page.parent_id
+		end
+
+		# add top of hierarchy to array 
+		# ASSUMES only one page has no parent_id
+		@last_page = pages.size
+		pages << @cms_site.pages.find_by(parent_id: nil).id
+			
+	end
 end
