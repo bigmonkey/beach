066ec2646b82f779a72723ddcd7cf985d18eb850
bigmonkey <conway@xerpi.com>
2013-11-17 23:06:55 -0500

    finished landing page, saves to table, error messages

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,11 +1,22 @@
 class ServicesController < ApplicationController
 	layout 'landing_homepage'
 
-def rehab
-	@guest = Guest.new
-end
+	def rehab
+		@guest = Guest.new
+	end	
 
-def thanks
-	
-end
+	def thanks
+
+		@guest= Guest.new(guest_params)	
+
+		@guest.save ? (@guestSaved = true) : (@guestSaved=false)	
+
+	end
+
+	private
+
+	def guest_params
+		params.require(:guest).permit(:name, :email, :phone, :comments) 
+	end 
+
 end
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,8 +1,10 @@
 class Guest < ActiveRecord::Base
 	validates :name, presence: true
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX, message: "appears to be invalid." }
 	validates :phone, presence: true
-	validates :phone, format: { with: /\d{3}-\d{3}-\d{4}/, message: "phone?" }
+	validates :phone, format: { with: /\d{3}-\d{3}-\d{4}/, message: "appears not to be in xxx-xxx-xxxx format." }
 	before_save { email.downcase! }
+
+
 end
